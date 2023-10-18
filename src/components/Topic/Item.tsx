012599cf75a89444525c87fa9397712595908eda
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-10-18 13:34:00 +0900

    ✨ feat(home) - home 퍼블리싱 - 토픽 아이템 공통 컴포넌트로 분리

---
+++ src/components/Topic/Item.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const Item = styled.li`
+	padding: 16px 48px;
+	font-weight: 900;
+	text-align: center;
+
+	color: #383b40;
+
+	background: #f2f4f6;
+	border-radius: 32px;
+	cursor: pointer;
+
+	&:hover {
+		box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.16);
+	}
+
+	&:active {
+		box-shadow: none;
+		background: var(--defalut, #e7edfd);
+	}
+`
+const TopicItem = ({ title }: { title: string }) => {
+	return <Item>{title}</Item>
+}
+
+export default TopicItem
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,63 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import styled from 'styled-components'
 //-Components
 import Layout from 'layout'
+import LargeButton from 'components/Button/Large'
+import TopicItem from 'components/Topic/Item'
+//-Icon
+import LogoSVG from 'public/app/logo.svg'
+import DambiSVG from 'public/icons/home_dambi.svg'
+import HotSVG from 'public/icons/hot.svg'
 
 const IndexPage: NextPage = () => {
 	return (
-		<Layout>
-			<div className='container mx-auto mt-12 md:mt-24 text-center'>
-				<span>메인페이지</span>
-			</div>
-		</Layout>
+		<>
+			<Layout>
+				<Box>
+					<LogoSVG />
+					<div className='mt-5 mb-2 font-extrabold text-center text-[#383b40] text-[20px] leading-8'>
+						담담은 AI 담비와 함께 학습하고 즐기는 <br />
+						<span className='text-[#324478] font-extrabold text-[20px] leading-8'>건강하고 자유로운 토의·토론 </span>
+						플랫폼입니다!
+					</div>
+					<DambiSVG />
+				</Box>
+
+				<div className='flex flex-col items-center justify-center'>
+					<div className='flex items-center justify-center gap-4 mt-10 mb-8'>
+						<HotSVG />
+						<div className='font-extrabold'>
+							<span className='text-[20px]'>오늘의 </span>
+							<span className='text-[#ed7743] text-[20px]'>HOT 토픽</span>
+						</div>
+					</div>
+
+					<ul className='flex flex-col items-center justify-center gap-4'>
+						<TopicItem title={'오늘의 인기 토픽 관련 질문 추천 🙋‍♀️'} />
+						<TopicItem title={'기후 변화와 지속 가능성 ☀️'} />
+						<TopicItem title={'온라인 학습 및 원격 교육 💻'} />
+						<TopicItem title={'사이버 보안 및 데이터 개인 정보 보호 🔒'} />
+						<TopicItem title={'생물 다양성 및 보전 🥀'} />
+					</ul>
+
+					<Link href='/topic' className='my-10'>
+						<LargeButton text={'토픽 더보기'} />
+					</Link>
+				</div>
+			</Layout>
+		</>
 	)
 }
 
 export default IndexPage
+
+const Box = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	padding-top: 36px;
+	border: 1px solid #e5e8ec;
+`
