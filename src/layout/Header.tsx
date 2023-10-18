793266a1ebd02f2f69de4586a57a87ebfe8fc114
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-10-18 12:02:46 +0900

    🎨 design(header) - 로고 사이즈 수정 - navigation item whitespace no-wrap 설정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -18,31 +18,31 @@ export default function Header() {
 
 	return (
 		<div id='header' className={isMobile ? 'small' : ''}>
-			<div className='w-full lg:container mx-auto'>
-				<div className='logo-box w-40'>
+			<div className='w-full mx-auto lg:container'>
+				<div className='w-40 logo-box'>
 					<Link href='/'>
-						<LogoSVG height={isMobile ? 28 : 40} />
+						<LogoSVG height={isMobile ? 16 : 28} />
 					</Link>
 				</div>
 				{!isMobile && (
 					<div className='flex flex-row items-start gap-24'>
 						<Link href='/study'>
-							<span className='text-xl text-main-900 font-extrabold'>학습</span>
+							<span className='text-xl font-extrabold text-main-900 whitespace-nowrap'>학습</span>
 						</Link>
 						<Link href='/topic'>
-							<span className='text-xl text-main-900 font-extrabold'>토픽</span>
+							<span className='text-xl font-extrabold text-main-900 whitespace-nowrap'>토픽</span>
 						</Link>
 						<Link href='/debate' className='relative'>
-							<span className='text-xl text-point-900 font-extrabold'>토론</span>
+							<span className='text-xl font-extrabold text-point-900 whitespace-nowrap'>토론</span>
 							<div className='w-full border-b-[3px] border-point-900 rounded-full' />
 							<NavPointSVG width={10} height={9} className='absolute top-0 -right-2' />
 						</Link>
 					</div>
 				)}
 
-				<div className='w-40 flex flex-row justify-end'>
+				<div className='flex flex-row justify-end w-40'>
 					{isMobile && <MenuSVG width={24} height={24} onClick={() => setModal(true)} />}
-					{!isMobile && <CharSVG width={48} height={48} />}
+					{!isMobile && <CharSVG width={36} height={36} />}
 				</div>
 			</div>
 
@@ -67,15 +67,15 @@ export default function Header() {
 						<CloseSVG width={24} height={24} className='cursor-pointer' onClick={() => setModal(false)} />
 					</div>
 
-					<div className='grow flex flex-col'>
+					<div className='flex flex-col grow'>
 						<Link href='/study'>
-							<span className='block w-full py-4 text-base text-main-900 font-extrabold'>학습</span>
+							<span className='block w-full py-4 text-base font-extrabold text-main-900'>학습</span>
 						</Link>
 						<Link href='/topic'>
-							<span className='block w-full py-4 text-base text-main-900 font-extrabold'>토픽</span>
+							<span className='block w-full py-4 text-base font-extrabold text-main-900'>토픽</span>
 						</Link>
 						<Link href='/debate'>
-							<span className='block w-full py-4 text-base text-main-900 font-extrabold'>토론</span>
+							<span className='block w-full py-4 text-base font-extrabold text-main-900'>토론</span>
 						</Link>
 					</div>
 				</div>
