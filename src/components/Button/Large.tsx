d90a4a0ead61c60f90f650db5126acbc69fb3f58
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-11-05 19:45:03 +0900

    🎨  design(button): 공통 컴포넌트 디자인 수정

--- src/components/Button/Large.tsx
+++ src/components/Button/Large.tsx
@@ -1,16 +1,17 @@
 import styled from 'styled-components'
 
-export const ButtonStyle = styled.button<{ width: string }>`
-	padding: 13px 0;
-	width: ${(props) => props.width}px;
+export const ButtonStyle = styled.button`
+	width: 100%;
+	padding: 1.4rem 0;
 
-	color: white;
+	font-size: 1.6rem;
 	font-weight: 800;
-	line-height: 30px;
+	line-height: 3rem;
 	text-align: center;
 
+	color: white;
 	background: linear-gradient(180deg, #515f8d 0%, #3b4b7e 100%);
-	border-radius: 30px;
+	border-radius: 3rem;
 
 	&:hover {
 		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -20,9 +21,14 @@ export const ButtonStyle = styled.button<{ width: string }>`
 		background: linear-gradient(180deg, #324478 0%, #152750 100%);
 		box-shadow: none;
 	}
+
+	@media screen and (max-width: 768px) {
+		font-size: 1.4rem;
+		padding: 1rem 0;
+	}
 `
-const LargeButton = ({ text, width }: { text: string; width?: string }) => {
-	return <ButtonStyle width={width || '464'}>{text}</ButtonStyle>
+const LargeButton = ({ text, onClick }: { text: string; onClick?: () => void }) => {
+	return <ButtonStyle onClick={onClick}>{text}</ButtonStyle>
 }
 
 export default LargeButton
