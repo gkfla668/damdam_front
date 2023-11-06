db139d7829680146b64a99f24e5a56e2600a288a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-11-06 17:36:26 +0900

    ✨ feat: 아이디/비밀번호 찾기 페이지 추가

---
+++ src/pages/findID/index.tsx
@@ -0,0 +1,7 @@
+import Layout from 'layout'
+
+const FindID = () => {
+	return <Layout>아이디 찾기 페이지입니다.</Layout>
+}
+
+export default FindID
---
+++ src/pages/findPW/index.tsx
@@ -0,0 +1,7 @@
+import Layout from 'layout'
+
+const FindPW = () => {
+	return <Layout>비밀번호 찾기 페이지입니다.</Layout>
+}
+
+export default FindPW
