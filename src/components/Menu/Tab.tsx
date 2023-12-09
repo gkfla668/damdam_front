7afa99d1bdc4f97e3d1a3f9245b9c3d15fc3a786
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-12-09 15:21:34 +0900

    🎨 design

--- src/components/Menu/Tab.tsx
+++ src/components/Menu/Tab.tsx
@@ -20,15 +20,15 @@ const MenuTab = ({ list, active, onClick, className }: Props) => {
 				<div
 					key={`menu-${value}`}
 					onClick={() => onClick && onClick(value)}
-					className='group grow flex-shrink-0 transition-all ease-in-out duration-150'
+					className='flex-shrink-0 transition-all duration-150 ease-in-out group grow'
 				>
 					<button
-						className={`w-full flex flex-col h-10 gap-3 items-center text-base group-hover:text-[#7F90BF] ${
-							active === value ? 'text-point-900 font-extrabold' : `text-[#747983] font-normal`
+						className={` w-full flex flex-col h-10 gap-3 items-center text-base md:text-lg  ${
+							active === value ? 'text-blue font-extrabold ' : `text-[#747983] font-normal group-hover:text-[#7F90BF]`
 						}`}
 					>
 						{label}
-						{active === value && <motion.div layoutId='menu-underline' className='w-full h-[1px] border-b-4 border-point-900' animate />}
+						{active === value && <motion.div layoutId='menu-underline' className=' w-full h-[1px] border-b-4 border-blue' animate />}
 						{active !== value && (
 							<div className='w-full h-[1px] border-b group-hover:border-b-4 mt-[3px] group-hover:mt-0 border-[#E5E8EC] group-hover:border-[#7F90BF] transition-all ease-in-out duration-150' />
 						)}
